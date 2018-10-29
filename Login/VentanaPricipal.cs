7e2e1dc88eee1769d9ec1f000ec86e69fe74c989
Jose <josemac020634@gmail.com>
2018-10-28 20:19:10 -0500

    htrhfghfg

--- Login/VentanaPricipal.cs
+++ Login/VentanaPricipal.cs
@@ -94,6 +94,9 @@ namespace Login
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+            //sdflknsdlfnsd
+            //sdflsdfjsd
+            //sdfklndslfnsdnfk
         }
     }
 }
